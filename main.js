4b53e265e5ffbd63ddccc23a341cc87fa6ca5881
spider-Monty <montyaction@hotmail.com>
2023-06-12 18:14:34 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+const calculator = document.querySelector('.calculator');
+const display = calculator.querySelector('.calculator__display');
+const keys = calculator.querySelector('.calculator__keys');
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === 'operator') {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+      console.log(displayedNum);
+      console.log(keyContent);
+    }
+
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.'
+    }
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-depressed')
+
+      // Add custom attribute
+      calculator.dataset.previousKeyType = 'operator';
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+
+    const calculate = (n1, operator, n2) => {
+      console.log(n1, operator, n2);
+      let result = "";
+
+      if (operator === "add") {
+        result = parseFloat(n1) + parseFloat(n2);
+      } else if (operator === "subtract") {
+        result = parseFloat(n1) - parseFloat(n2);
+      } else if (operator === "multiply") {
+        result = parseFloat(n1) * parseFloat(n2);
+      } else if (operator === "divide") {
+        result = parseFloat(n1) / parseFloat(n2);
+      }
+
+      return result;
+    };
+
+    if (action === 'calculate') {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+      console.log(firstValue, operator, secondValue);
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
+
+    // Remove .is-depressed class from all keys
+    Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+  }
+
+
+  // if (action === 'decimal') {
+  //   console.log('decimal key!')
+  // }
+
+  // if (action === 'clear') {
+  //   console.log('clear key!')
+  // }
+
+})
